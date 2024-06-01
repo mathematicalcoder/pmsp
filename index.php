b98cafa404438f271440f5f07928ff5a786597f5
Kaiser Chan <106592999+mathematicalcoder@users.noreply.github.com>
2024-06-01 15:17:03 +0800

    Update index.php

--- index.php
+++ index.php
@@ -7,27 +7,8 @@
   </head>
   <body>
     <!-- navbar -->
-<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Logo</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="collapsibleNavbar">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link" href="https://mathematicalcoder.github.io/pmsp/">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="https://mathematicalcoder.github.io/pmsp/about.php">About</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Login</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
+<?php include("https://mathematicalcoder.github.io/pmsp/assets/navbar.php"); ?>
+
 </nav>
 
     <div class="container-xl mt-3 border">
